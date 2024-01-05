50f2100838310f1a99457f618fb82d87539b33d1
Smith Ringtho <sringtho@gmail.com>
2024-01-05 18:46:57 +0300

    Feat: Added Product mutations and queries

---
+++ frontend/src/redux/api/productApiSlice.js
@@ -0,0 +1,86 @@
+import { apiSlice } from './apiSlice'
+import { PRODUCTS_URL, UPLOAD_URL } from '../constants'
+
+export const productApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: ({ keyword }) => ({
+        url: `${PRODUCTS_URL}`,
+        params: { keyword },
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: ['Product']
+    }),
+    getProductById: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      providesTags: (result, error, productId) => [
+        { type: "Product", id: productId }
+      ]
+    }),
+    allProducts: builder.query({
+        query: () => `$${PRODUCTS_URL}/allproducts`,
+    }),
+    getProductDetails: builder.query({
+        query: (productId) => ({
+            url: `${PRODUCTS_URL}/${productId}`
+        }),
+        keepUnusedDataFor: 5
+    }),
+    createProduct: builder.mutation({
+        query: (productData) => ({
+            url: `${PRODUCTS_URL}`,
+            method: 'POST',
+            body: productData
+        }),
+        invalidatesTags: ['Product']
+    }),
+    updateProduct: builder.mutation({
+        query: ({ productId, formData }) => ({
+            url: `${PRODUCTS_URL}/${productId}`,
+            method: 'PUT',
+            body: formData
+        })
+    }),
+
+    deleteProduct: builder.mutation({
+        query: (productId) => ({
+            url: `${PRODUCTS_URL}/${productId}`,
+            method: 'DELETE'
+        }),
+        providesTags: ['Product']
+    }),
+    createReview: builder.mutation({
+        query: (data) => ({
+            url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+            method: 'POST',
+            body: data
+        })
+    }),
+
+    getTopProducts: builder.query({
+        query: () => `${PRODUCTS_URL}/top`,
+        keepUnusedDataFor: 5
+    }),
+
+    getNewProducts: builder.query({
+        query: () => `${PRODUCTS_URL}/new`,
+        keepUnusedDataFor: 5
+    }),
+
+  }),
+})
+
+export const {
+    useAllProductsQuery,
+    useCreateProductMutation,
+    useCreateReviewMutation,
+    useDeleteProductMutation,
+    useGetNewProductsQuery,
+    useGetProductByIdQuery,
+    useGetProductDetailsQuery,
+    useGetProductsQuery,
+    useGetTopProductsQuery,
+    useUpdateProductMutation,
+} = productApiSlice
--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,3 +1,5 @@
 export const BASE_URL = '' //proxy
 export const USERS_URL = '/api/v1/users'
 export const CATEGORIES_URL = '/api/v1/categories'
+export const PRODUCTS_URL = '/api/v1/products'
+export const UPLOAD_URL = '/api/uploads'
